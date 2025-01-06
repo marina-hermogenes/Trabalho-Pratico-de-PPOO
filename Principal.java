512c68917c64aff6c5b8048fe9569e89fe7b3428
livia-della-garza <liviadgarza@gmail.com>
2025-01-06 18:38:30 -0300

    Código inicial

---
+++ Principal.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Luiz Merschmann
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Simulacao sim = new Simulacao();
+        sim.executarSimulacao(40);
+    }
+}
